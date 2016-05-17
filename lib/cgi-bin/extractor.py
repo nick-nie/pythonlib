43ea079be88dd92e9da12eb53a53f735af0647cf
Nick Nie <Nick_Nie@epam.com>
2016-05-17 18:47:34 +0800

    done

---
+++ lib/cgi-bin/extractor.py
@@ -0,0 +1,37 @@
+import cgi
+from bs4 import BeautifulSoup
+import urllib2
+import ssl
+import pathlib
+
+print 'Content-type: text/html\n\n'
+print '<H3>URL Extractor</H3>'
+print 
+form = cgi.FieldStorage()
+
+protocol = form['protocol'].value
+host = form['host'].value
+port = form['port'].value
+pattern = form['pattern'].value
+dir = form['dir'].value
+file = form['file'].value
+
+# pattern = '/store/powertools/en/USD/Open-Catalogue/'
+url = str(protocol + '://' + host + ':' + port + dir) # write the url here
+print 
+gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+usock = urllib2.urlopen(url, context=gcontext)
+data = usock.read()
+usock.close()
+soup = BeautifulSoup(data, 'html.parser')
+
+with open(str(file), 'wt') as wf:
+    for link in soup.find_all('a'):
+        tmp = str(link.get('href'))
+        if tmp.startswith(pattern): 
+	    print '%s<br>' % tmp
+	    wf.write(tmp + '\n')
+    wf.close()
+fileUrl = pathlib.Path(file).as_uri()
+print '<H3>URL generated in <a href = fileUrl>%s</a></H3>' % fileUrl
+
